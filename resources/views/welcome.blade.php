e848dd18661c4fc980f12916191e124a1fc0b8f3
Tommaso Simone <tsimone9838@gmail.com>
2022-06-07 16:55:37 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 offset-md-3">
+                <h1>Benvenuti nello studio oculistico</h1>
+            </div>
+        </div>
+    </div>
+</x-layout>
